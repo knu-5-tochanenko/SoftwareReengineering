51a96e4d76c4d2822c9e2d83ec196fcb7218dbcf
Vladyslav Tochanenko <itochanenko@gmail.com>
2022-05-27 14:36:45 +0300

    Rename elements on Main (Authorization) Screen

--- Lab_1/Refactored/authorization.h
+++ Lab_1/Refactored/authorization.h
@@ -19,9 +19,9 @@ public:
     void readSettings();
 
 private slots:
-    void on_pushButton_clicked();
-    void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
+    void on_pushButton_continue_clicked();
+    void on_pushButton_exit_clicked();
+    void on_pushButton_newGame_clicked();
 
 private:
     Ui::authorization *ui;
