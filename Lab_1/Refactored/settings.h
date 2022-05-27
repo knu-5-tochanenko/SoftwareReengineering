97bd11323b21b05edc2ff55a5793b4a355b87200
Vladyslav Tochanenko <itochanenko@gmail.com>
2022-05-27 18:12:17 +0300

    Rename Setting elements

--- Lab_1/Refactored/settings.h
+++ Lab_1/Refactored/settings.h
@@ -17,8 +17,8 @@ public:
     ~Settings();
 
 private slots:
-    void on_pushButton_clicked();
-    void on_pushButton_2_clicked();
+    void on_pushButton_continue_clicked();
+    void on_pushButton_goBack_clicked();
 
 private:
     Ui::Settings *ui;
